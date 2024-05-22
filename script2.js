092678b37f5e6f7849f2a6ea4b47b35a5eb5d9a3
PEDRO GEORGE DA SILVA LOPES <pedro.george@a.ficr.edu.br>
2024-05-22 20:34:53 -0300

    adicionado os que estavam faltando e foram esquecidos, e ultimas finalizações

---
+++ script2.js
@@ -0,0 +1,59 @@
+// chamdando variaveis
+const progressBar = document.getElementById("progressBar");
+const buttonPlay = document.querySelector('#play');
+const buttonPause = document.querySelector('#pause');
+const tempoAtual = document.getElementById("tempoAtual");
+const tempoTotal = document.getElementById("tempoTotal");
+
+const buttonVoltar = document.querySelector('#voltar');
+const buttonAvançar = document.querySelector('#avancar');
+
+// metodo de audio
+
+const music = new Audio('./assets/Bate Forte e Dança (Super Slowed) (320).mp3');
+let interval;
+
+//funçoes
+
+function formatarTempo(segundos) {
+  const min = Math.floor(segundos / 60);
+  const seg = Math.floor(segundos % 60);
+  return `${min.toString().padStart(2, '0')}:${seg.toString().padStart(2, '0')}`;
+}
+
+function updateMusicTime() {
+  const progresso = (music.currentTime / music.duration) * 100;
+  progressBar.value = progresso;
+  tempoAtual.textContent = formatarTempo(music.currentTime);
+}
+
+music.addEventListener('loadedmetadata', function () {
+  tempoTotal.textContent = formatarTempo(music.duration);
+});
+
+buttonVoltar.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
+
+buttonAvançar.addEventListener('click', () => {
+  window.location.href = 'index3.html';
+});
+
+
+
+function play() {
+  buttonPlay.classList.add('hide');
+  buttonPause.classList.remove('hide');
+  music.play();
+  interval = setInterval(updateMusicTime, 1000);
+}
+
+function pause() {
+  buttonPlay.classList.remove('hide');
+  buttonPause.classList.add('hide');
+  music.pause();
+}
+
+
+buttonPlay.addEventListener('click', play);
+buttonPause.addEventListener('click', pause);
